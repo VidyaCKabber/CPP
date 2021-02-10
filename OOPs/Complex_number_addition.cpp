d6b82a92d1d3fe4c4c97bc2371b1db49218ec859
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2021-02-10 10:09:57 +0530

    Create Complex_number_addition.cpp

---
+++ OOPs/Complex_number_addition.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+class Complex{
+private:
+    int real;
+    int imag;
+
+public:
+    Complex(int r=0,int i=0){
+        real = r;
+        imag = i;
+    }
+
+    Complex operator +(Complex const &obj){
+        Complex res;
+        cout << real << endl;
+        cout << obj.real << endl;
+        res.real = real + obj.real;
+        res.imag = imag + obj.imag;
+        return res;
+    }
+
+    void print(){
+        cout << real << "+" << imag << "i" << endl;
+    }
+};
+
+int main(){
+    Complex c1(10,2),c2(21,3);
+    Complex c3 = c1 + c2;
+    c3.print();
+}
