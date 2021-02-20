226747178d93d625b0c73e3c3a20cc58eb83b045
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2021-02-20 20:32:11 +0530

    Update Lower_Bound_STL.cpp

--- HackerRank/Lower_Bound_STL.cpp
+++ HackerRank/Lower_Bound_STL.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-
 int main() {   
     int N,Q,num,inputs;
     vector<int>arr; 
@@ -35,3 +34,42 @@ int main() {
     }
     return 0;
 }
+
+# -----------------------------------OR-----------------------------------------------
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {   
+    int N,Q,num,inputs;
+    vector<int>arr; 
+    vector<int>::iterator is_ele_present,lower;
+    
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> num;
+        arr.push_back(num);        
+    }
+    
+    cin >> Q;
+    //sort(arr.begin(), arr.end());
+    for(int j=0; j<Q; j++){
+        cin >> inputs;
+        lower = lower_bound(arr.begin(), arr.end(), inputs); 
+        
+        if (*(lower + 1) == inputs || *lower == inputs){
+            cout << "Yes "<< (lower - arr.begin())+1 << endl;
+        }
+        else{
+            cout << "No " << (lower - arr.begin()+1) << endl;
+        }     
+    }
+    return 0;
+}
+
