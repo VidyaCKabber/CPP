f4c20b2d584ca29b9b29efd0ba946f3208d106f7
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2021-04-02 21:51:22 +0530

    Update Hybrid_Inheritance.cpp

--- OOPs/Hybrid_Inheritance.cpp
+++ OOPs/Hybrid_Inheritance.cpp
@@ -1,20 +1,23 @@
-
 #include <iostream>
 using namespace std;
-
 class Vehicle{
     public:
     Vehicle(){
         cout << "This is super class method" << endl;
     }
 };
+class Fare{
+    public:
+    Fare(){
+        cout << "This is fare class" <<endl;
+    }
+};
 class Car : public Vehicle{
     
 };
-class Bike : public Vehicle{
+class Bike : public Vehicle, public Fare{
     
 };
-
 int main()
 {
     Bike b;
