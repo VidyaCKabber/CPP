575b049e2566c49b07cc02b74ed20c10e157c453
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2021-04-10 19:26:43 +0530

    Create FriendClass.cpp

---
+++ OOPs/FriendClass.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+using namespace std;
+
+class FriendClassExample{
+    private:
+        int x = 10;
+        char a = 'V';
+        
+    public:
+        friend class ABC; 
+};
+
+class ABC{
+    public:
+    void show(FriendClassExample b){
+        cout << b.x << endl;
+        cout << b.a << endl;
+    }
+};
+
+
+int main()
+{
+    FriendClassExample obj;
+    
+    ABC obj1;
+    
+    obj1.show(obj);
+
+    return 0;
+}
