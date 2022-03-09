793b3879791ea73d6fc5219cd6194022d1eae82c
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2022-03-09 19:38:50 +0530

    Create Problem_7.cpp

---
+++ Problems/Problem_7.cpp
@@ -0,0 +1,49 @@
+// C++ program to read and print students information using two classes and simple inheritance
+#include <iostream>
+using namespace std;
+
+class Student_Basic_Info{
+protected:
+    char name[20];
+    char usn[10];
+    int age;
+    char gender[10];
+
+public:
+    void get_student_info(){
+        cout << "Enter student name : ";
+        cin >> name;
+        cout << "Enter student usn : ";
+        cin >> usn;
+        cout << "Enter student age : ";
+        cin >> age;
+        cout << "Enter student gender : ";
+        cin >> gender;
+    }
+};
+
+class Student_Result_Info : public Student_Basic_Info{
+private:
+    int marks;
+public:
+    void read_student_marks(){
+        cout << "Enter Student total 6 subject marks";
+        cin >> marks;
+    }
+
+    void display_student_info(){
+        cout << "Student's Name : "<< name << endl;
+        cout << "Student's USN : "<< usn << endl;
+        cout << "Student's Age : "<< age << endl;
+        cout << "Student's Gender : "<< gender << endl;
+        cout << "Student's Percentage : "<< (marks/6)*100 << endl;
+    }
+};
+
+int main(){
+    Student_Result_Info student;
+    student.get_student_info();
+    student.read_student_marks();
+    student.display_student_info();
+}
+
